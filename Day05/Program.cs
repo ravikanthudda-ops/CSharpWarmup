853dc36adc033016bb550a1f53fdd1d1000c7dac
ravikanthooda <ravikanthooda@outlook.com>
2026-05-25 19:05:23 +0530

    Day05-Done

---
+++ Day05/Program.cs
@@ -0,0 +1,15 @@
+﻿class LargestOfTwo
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter two numbers");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+        if(num1>num2)
+            Console.WriteLine($"Num {num1} is largest");
+        else if(num2>num1)
+            Console.WriteLine($"Num {num2} is largest");
+        else
+            Console.WriteLine("Both are same");
+    }
+}
