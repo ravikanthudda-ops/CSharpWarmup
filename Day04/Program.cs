8ee4d23918f34df2116d9db69178b91a420ddd07
ravikanthooda <ravikanthooda@outlook.com>
2026-05-25 18:55:39 +0530

    Day04-Done

---
+++ Day04/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class EvenOdd
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter any number to check even/odd");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if(number%2==0)
+            Console.WriteLine("Even");
+        else
+            Console.WriteLine("Odd");
+    }
+}
