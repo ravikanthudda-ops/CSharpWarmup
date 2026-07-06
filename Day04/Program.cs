79715cd9a817f220a49949fd5917629d0d6cff89
ravikanthooda <ravikanthooda@outlook.com>
2026-07-07 01:39:01 +0530

    Day02-Day04 done

---
+++ Day04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class EvenOdd
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter number");
+        string num = Console.ReadLine()??string.Empty;
+        if(int.TryParse(num, out int number))
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+    }
+}
