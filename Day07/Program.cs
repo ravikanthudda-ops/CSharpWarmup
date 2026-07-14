c29f15b81b606ae2ff099c1ad1a7c1aa1f549008
ravikanthooda <ravikanthooda@outlook.com>
2026-07-15 00:51:00 +0530

    Day07 done

---
+++ Day07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class LeapYear
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter any year");
+        string year = Console.ReadLine()??string.Empty;
+        if(int.TryParse(year,out int yy))
+        {
+            if(yy%4==0 && (yy%100!=0 || yy%400==0))
+            {
+                    Console.WriteLine("Leap Year");             
+            }
+            else
+            {
+                Console.WriteLine("Not Leap Year");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid Year");
+        }
+    }
+}
