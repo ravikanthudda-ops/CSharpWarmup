3438cd0024285de80a5cd056cce241f91a647713
ravikanthooda <ravikanthooda@outlook.com>
2026-08-07 00:20:20 +0530

    Day02 Done

---
+++ Practice/Day02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class NameAndAge
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name");
+        string name = Console.ReadLine()??string.Empty;
+        Console.WriteLine("Enter your age");
+        string age = Console.ReadLine()??string.Empty;
+        Console.WriteLine($"Hi, {name}. You are {age} old");
+    }
+}
