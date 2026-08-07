8de3f12d7084e0f7816caac4a472a7cbdd263655
ravikanthooda <ravikanthooda@outlook.com>
2026-08-08 00:38:20 +0530

    Day05 done

---
+++ Day05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class LargestNum
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter first number");
+        string number1 = Console.ReadLine()??string.Empty;
+        Console.WriteLine("Enter second number");
+        string number2 = Console.ReadLine()??string.Empty;
+
+        if(int.TryParse(number1, out int num1) && int.TryParse(number2, out int num2))
+        {
+            if(num1<num2)
+            {
+                Console.WriteLine($"{num2} is larger than {num1}");
+            }
+            if(num1>num2)
+            {
+                Console.WriteLine($"{num1} is larger than {num2}");
+            }
+            if(num1==num2)
+            {
+                Console.WriteLine("Both numbers are equal");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Numbers are not valid");
+        }
+    }
+}
